0fa6ea8ebd3b7f65b722c2b80b0c1e3b7411008e
Camilo Neira Gómez <camiloneira93@hotmail.com>
2023-02-27 14:58:11 -0500

    Se actualiza el proyecto

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* Swipper (Carrusel) */
+
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
   spaceBetween: 25,
@@ -28,20 +30,45 @@ var swiper = new Swiper(".slide-content", {
   },
 });
 
+/* Arrays */
 let carrito = [];
 let productos = [];
 
+/* Productos */
+
 productos.push(new Producto("0", "Blusa", 30000, "./imagenes/Camiseta1.jpeg"));
 productos.push(new Producto("1", "Blusa", 50000, "./imagenes/Camiseta2.jpeg"));
-productos.push(new Producto("2", "Blusa", 30000, "./imagenes/Pantalon.jpeg"));
+productos.push(new Producto("2", "Blusa", 30000, "./imagenes/Camiseta3.jpeg"));
 productos.push(
-  new Producto("3", "Pantalón", 60000, "./imagenes/Pantalon2.jpeg")
+  new Producto("3", "Pantalón", 60000, "./imagenes/Pantalon.jpeg")
 );
+
+/* Elementos Traidos del DOM */
 
 const contenedorProductos = document.getElementById("contenedorProductos");
 const modalBody = document.getElementById("modalCar");
+const precioTotal = document.getElementById("precioTotal");
+const loaderTexto = document.getElementById("loaderTexto");
+const loader = document.getElementById("loader");
+const botonFinalizarCompra = document.getElementById("botonFinalizarCompra");
 
-window.addEventListener("load", loadProductos);
+/* Load Productos */
+
+setTimeout(() => {
+  loaderTexto.remove();
+  loader.remove();
+  loadProductos();
+}, 2000);
+
+/* Escuchadores (eventos) */
+
+/* window.addEventListener("load", loadProductos); */
+modalBody.addEventListener("click", agregarAlCarrito(carrito));
+botonFinalizarCompra.addEventListener("click", () => {
+  finalizarCompra(carrito);
+});
+
+/* Funciones */
 
 function loadProductos() {
   productos.forEach((producto) => {
@@ -99,42 +126,130 @@ function loadProductos() {
       carrito.push(productoSeleccionado);
       localStorage.setItem("carrito", JSON.stringify(carrito));
       agregarAlCarrito(carrito);
+
+      Swal.fire({
+        title: "Ha agregado un producto al carrito",
+        text: "Estás a punto de llevar la mejor ropa contigo!",
+        icon: "success",
+        confirmButtonColor: "warning",
+        confirmButtonText: "Gracias!",
+        timer: 3000,
+      });
     });
   }
+  agregarAlCarrito(carrito);
 }
 
-modalBody.addEventListener("click", agregarAlCarrito);
+//cargar el carrito si es que hay algo
+localStorage.getItem("carrito")
+  ? (carrito = JSON.parse(localStorage.getItem("carrito")))
+  : ((carrito = []), localStorage.setItem("carrito", JSON.stringify(carrito)));
 
-console.log(carrito);
-
-function agregarAlCarrito(carrito) {
+function agregarAlCarrito(array) {
   modalBody.innerHTML = "";
 
   let subtotal = 0;
   let total = 0;
   let cantidadTotal = 0;
 
-  carrito.forEach((producto) => {
-    const img = document.createElement("img");
-    img.src = producto.imagen;
-    img.className = "card-image";
-
+  array.forEach((productoCarrito) => {
     const cardContent = document.createElement("div");
     cardContent.className = "card-content";
+    cardContent.setAttribute("id", `productoCarrito${productoCarrito.id}`);
+
+    const img = document.createElement("img");
+    img.src = productoCarrito.imagen;
+    img.className = "card-image";
 
     const h4 = document.createElement("h4");
     h4.className = "name";
-    h4.innerText = producto.nombre;
+    h4.innerText = productoCarrito.nombre;
 
     const p = document.createElement("p");
     p.className = "description";
-    p.innerText = "$" + producto.precio + " COP";
+    p.innerText = "$" + productoCarrito.precio + " COP";
 
-    modalBody.appendChild(img);
+    let botonE = document.createElement("i");
+    botonE.classList.add("fa-solid", "fa-trash");
+    botonE.setAttribute(`id`, `btnEliminar${productoCarrito.id}`);
+
+    cardContent.appendChild(img);
 
     cardContent.appendChild(h4);
     cardContent.appendChild(p);
+    cardContent.appendChild(botonE);
 
     modalBody.appendChild(cardContent);
+  });
+
+  array.forEach((productoCarrito) =>
+    document
+      .getElementById(`btnEliminar${productoCarrito.id}`)
+      .addEventListener("click", () => {
+        let cardContent = document.getElementById(
+          `productoCarrito${productoCarrito.id}`
+        );
+        cardContent.parentNode.removeChild(cardContent);
+
+        let indice = array.indexOf(productoCarrito);
+        array.splice(indice, 1);
+
+        localStorage.setItem("carrito", JSON.stringify(array));
+
+        Swal.fire({
+          title: "Eliminaste un producto del carrito",
+          text: "Vuelve pronto para que nos lleves contigo!",
+          icon: "error",
+          confirmButtonColor: "warning",
+          confirmButtonText: "Gracias!",
+          timer: 3000,
+        });
+        calcularTotal(array);
+      })
+  );
+  calcularTotal(array);
+}
+
+function calcularTotal(array) {
+  let total = array.reduce(
+    (acc, productoCarrito) => acc + productoCarrito.precio,
+    0
+  );
+  total == 0
+    ? (precioTotal.innerHTML = `No hay productos en el carrito</strong>`)
+    : (precioTotal.innerHTML = `El total es: <strong>${total}</strong>`);
+}
+
+function finalizarCompra(array) {
+  Swal.fire({
+    title: "Está seguro de realizar la compra",
+    icon: "info",
+    showCancelButton: true,
+    confirmButtonText: "Sí, seguro",
+    cancelButtonText: "No, no quiero",
+    confirmButtonColor: "green",
+    cancelButtonColor: "red",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Compra realizada",
+        icon: "success",
+        confirmButtonColor: "green",
+        text: `Muchas gracias por su compra ha adquirido nuestros productos. `,
+      });
+      //resetear carrito
+      carrito = [];
+      //removemos storage
+      localStorage.setItem("carrito", []);
+      agregarAlCarrito(carrito);
+    } else {
+      Swal.fire({
+        title: "Compra no realizada",
+        icon: "info",
+        text: `La compra no ha sido realizada! Sus productos siguen en el carrito :D`,
+        confirmButtonColor: "green",
+        timer: 3500,
+      });
+    }
   });
 }
--- productos.js
+++ productos.js
@@ -6,3 +6,25 @@ class Producto {
     this.imagen = imagen;
   }
 }
+
+/* [
+  {
+    id: 1,
+    nombre: "Blusa",
+    precio: 30000,
+    imagen: "imagenes/Camiseta1.jpeg",
+  },
+  { id: 2, nombre: "Blusa", precio: 50000, imagen: "imagenes/Camiseta2.jpeg" },
+  {
+    id: 3,
+    nombre: "Blusa",
+    precio: 30000,
+    imagen: "imagenes/Camiseta3.jpeg",
+  },
+  {
+    id: 4,
+    nombre: "Pantalon",
+    precio: 30000,
+    imagen: "imagenes/Pantalon.jpeg",
+  },
+]; */
